a7f6f694a6f826e3001588185f518c855cc75633
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-10-05 20:49:06 +0500

    Update Data vis.py

--- ML /Data vis.py
+++ ML /Data vis.py
@@ -2,17 +2,42 @@ plt.figure()
 plt.hist(df["c1"])
 plt.show
 
+sns.histplot(df["c1"], bins=10, kde=True)
+plt.show
+#use for outlier detection (distributional skewness and extreme vals)
+#univariate
+#focus on understanding distribution, spread
+#find mean,meadian,mode
+
 x1=df["c1"]
 x2=["c2"]
 sns.scatterplot(x=x1,y=x2,hue["TARGET"])
 plt.show()
+#use for outlier detection (bivariate)
+#bivariate
+#correlation (numeric-numeric)
+#group difference (numeric-categorical)
+#scatter patterns
 
 sns.distplot(df["c2"])
 plt.show()
 
 sns.boxplot(df["c1"])
 plt.show()
+#use for outlier detection (univariate)
+#univariate
+#focus on understanding distribution, spread
+#find mean,meadian,mode
 
 sns.heatmap(data=df.corr(),annot=True)
 plt.show()
+#Multivariate 3 or more vars
+#understand complex interaction
+
+sns.pairplot(df)
+plt.show()
+#Multivariate 3 or more vars
+#understand complex interaction
+
+
 
