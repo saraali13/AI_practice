7074f7d178b8045e19c7611281fbf9f0790544c1
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-10-05 20:55:36 +0500

    Update KNN.py

--- ML /KNN.py
+++ ML /KNN.py
@@ -11,11 +11,28 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Normalize
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test
-knn = KNeighborsClassifier(n_neighbors=3)
+X_test = scaler.transform(X_test)
+
+#knn = KNeighborsClassifier(n_neighbors=3)
+#knn = KNeighborsClassifier(n_neighbors=3,metric="euclidean")
+#knn = KNeighborsClassifier(n_neighbors=3,algorithm="ball_tree")                        
+knn = KNeighborsClassifier()
 knn.fit(X_train, y_train)
 y_pred_knn = knn.predict(X_test)
+
+train_acc=knn.score(X_train,y_train)
+test_acc=accuracy_score(y_test,y_pred_knn)
 
 print("KNN Accuracy:", knn.score(X_test, y_test))
 print("\nConfusion Matrix:\n", confusion_matrix(y_test, y_pred_knn))
 print("\nClassification Report:\n", classification_report(y_test, y_pred_knn))
+
+#diff ks
+acc=[]
+for i in range(1,11):
+  knn2=KNeighborsClassifier(n_neighbors=i)
+  knn2.fit(X_train,y_train)
+  pred_y=knn.predict(X_test)
+  acc_=accuracy_score(y_test,pred_y)
+  acc.append(acc_)
+
