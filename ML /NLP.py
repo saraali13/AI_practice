8a18c19f688d2ccdcd30c59a72a7a6043c116c26
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-12-01 22:39:43 +0500

    Implement text tokenization and stop word removal
    Added functionality to tokenize text and remove stop words using NLTK.

--- ML /NLP.py
+++ ML /NLP.py
@@ -34,3 +34,15 @@ def clean_text(t):
     return " ".join(t)
 
 df['clean_text'] = df['text'].apply(clean_text)
+
+from nltk.corpus import stopwords
+
+nltk.download("stopwords")
+
+text="Hi, My name is Sara Ali. I am 19 years old. It felt nice meeting you"
+word_tk=word_tokenize(text)
+
+stop_words=set(stopwords.words("english"))
+
+fil_words=[word for word in word_tk if word.lower() not in stop_words]
+print(f"Original words: {word_tk}\nAfter removing stop words: {fil_words}")
